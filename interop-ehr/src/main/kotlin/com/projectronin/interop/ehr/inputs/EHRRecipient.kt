9c30e55c5cea8cbd9aa4463ed17b9c29f6a7dbb9
Beau Berger <96446663+beau-ronin@users.noreply.github.com>
2022-02-10 12:59:20 -0600

    Add default value to EHRRecipient (#32)

--- interop-ehr/src/main/kotlin/com/projectronin/interop/ehr/inputs/EHRRecipient.kt
+++ interop-ehr/src/main/kotlin/com/projectronin/interop/ehr/inputs/EHRRecipient.kt
@@ -5,4 +5,4 @@ import com.projectronin.interop.fhir.r4.datatype.Identifier
 /**
  * The [id] recipient of an EHR message, with a list of [Identifier]s
  */
-data class EHRRecipient(val id: String, val identifiers: List<Identifier>)
+data class EHRRecipient(val id: String, val identifiers: List<Identifier> = listOf())
