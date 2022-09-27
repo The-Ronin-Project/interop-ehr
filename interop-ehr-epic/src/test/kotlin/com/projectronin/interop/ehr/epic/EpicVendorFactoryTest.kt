2a42cc9299602db1431be390305d28ca61c13d79
Josh Smith <43484632+arssycro@users.noreply.github.com>
2022-09-27 16:10:55 -0500

    INT-1097: Add test for EpicVendorFactory to verify Spring can build it (#225)

--- interop-ehr-epic/src/test/kotlin/com/projectronin/interop/ehr/epic/EpicVendorFactoryTest.kt
+++ interop-ehr-epic/src/test/kotlin/com/projectronin/interop/ehr/epic/EpicVendorFactoryTest.kt
@@ -4,6 +4,12 @@ import com.projectronin.interop.common.vendor.VendorType
 import io.mockk.mockk
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
+import org.ktorm.database.Database
+import org.springframework.beans.factory.annotation.Qualifier
+import org.springframework.context.annotation.AnnotationConfigApplicationContext
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.ComponentScan
+import org.springframework.context.annotation.Configuration
 
 class EpicVendorFactoryTest {
     private val appointmentService = mockk<EpicAppointmentService>()
@@ -30,6 +36,22 @@ class EpicVendorFactoryTest {
             medicationService,
             medicationStatementService,
         )
+
+    @Test
+    fun `is all wired for spring`() {
+        @Configuration
+        @ComponentScan("com.projectronin.interop")
+        class TestConfig() {
+            @Bean
+            @Qualifier("ehr")
+            fun ehrDatabase(): Database = mockk()
+        }
+
+        val applicationContext = AnnotationConfigApplicationContext(TestConfig::class.java)
+
+        // If the Vendor Factory and its dependencies are not wired, this will throw an UnsatisfiedDependencyException.
+        applicationContext.getBean(EpicVendorFactory::class.java)
+    }
 
     @Test
     fun `vendor type is epic`() {
