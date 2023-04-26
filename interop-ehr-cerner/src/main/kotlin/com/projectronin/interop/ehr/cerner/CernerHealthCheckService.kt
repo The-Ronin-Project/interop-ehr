6f5c17785b9670df6b0cffe6d5f2ed7f21a12892
Josh Smith <43484632+arssycro@users.noreply.github.com>
2023-04-26 17:05:43 -0500

    INT-1602: Reduce log levels to prevent alerting (#462)

--- interop-ehr-cerner/src/main/kotlin/com/projectronin/interop/ehr/cerner/CernerHealthCheckService.kt
+++ interop-ehr-cerner/src/main/kotlin/com/projectronin/interop/ehr/cerner/CernerHealthCheckService.kt
@@ -7,6 +7,7 @@ import com.projectronin.interop.tenant.config.model.Tenant
 import mu.KotlinLogging
 import org.springframework.stereotype.Component
 import java.time.LocalDate
+
 @Component
 class CernerHealthCheckService(
     private val authorizationService: CernerAuthenticationService,
@@ -14,22 +15,24 @@ class CernerHealthCheckService(
     private val tenantService: TenantService
 ) : HealthCheckService {
     private val logger = KotlinLogging.logger { }
+
     override fun healthCheck(tenant: Tenant): Boolean {
         try {
             authorizationService.getAuthentication(tenant)
         } catch (e: Exception) {
-            logger.error(e) { "Failed authorization health check for ${tenant.mnemonic}" }
+            logger.debug(e) { "Failed authorization health check for ${tenant.mnemonic}" }
             return false
         }
         try {
             patientService.findPatient(tenant, LocalDate.now(), "Health", "Check")
         } catch (e: Exception) {
-            logger.error(e) { "Failed authorization health check for ${tenant.mnemonic}" }
+            logger.debug(e) { "Failed Patient search health check for ${tenant.mnemonic}" }
             return false
         }
         logger.debug { "Health check successful for ${tenant.mnemonic}" }
         return true
     }
 
-    override fun healthCheck(): Map<Tenant, Boolean> = tenantService.getMonitoredTenants().associateWith { healthCheck(it) }
+    override fun healthCheck(): Map<Tenant, Boolean> =
+        tenantService.getMonitoredTenants().associateWith { healthCheck(it) }
 }
