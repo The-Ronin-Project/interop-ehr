2e73de1f95b88617eb6d042ffcdff2bb7203ebb0
Steven Juana <50198085+StevenJuana@users.noreply.github.com>
2023-07-07 14:44:31 -0700

    [INT-1891] Updating interop-ehr to new dependency plugin and GHA (#555)

--- interop-fhir-ronin/interop-fhir-ronin.gradle.kts
+++ interop-fhir-ronin/interop-fhir-ronin.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    id("com.projectronin.interop.gradle.spring")
-    id("com.projectronin.interop.gradle.junit")
+    alias(libs.plugins.interop.gradle.spring)
+    alias(libs.plugins.interop.gradle.junit)
 }
 
 dependencies {
