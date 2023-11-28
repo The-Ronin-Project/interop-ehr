9d4367686433b0407a060c451c4e635b6006768a
Josh Smith <43484632+arssycro@users.noreply.github.com>
2023-11-28 15:27:24 -0600

    INT-2398: Make Cerner DocRef categories configurable (#690)

--- interop-ehr-cerner/src/main/kotlin/com/projectronin/interop/ehr/cerner/CernerDocumentReferenceService.kt
+++ interop-ehr-cerner/src/main/kotlin/com/projectronin/interop/ehr/cerner/CernerDocumentReferenceService.kt
@@ -5,6 +5,7 @@ import com.projectronin.interop.ehr.cerner.client.CernerClient
 import com.projectronin.interop.fhir.r4.resource.DocumentReference
 import com.projectronin.interop.tenant.config.model.Tenant
 import datadog.trace.api.Trace
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
 import java.time.LocalDate
 
@@ -12,9 +13,15 @@ import java.time.LocalDate
  * Service providing access to DocumentReferences within Cerner.
  */
 @Component
-class CernerDocumentReferenceService(cernerClient: CernerClient) : DocumentReferenceService, CernerFHIRService<DocumentReference>(cernerClient) {
+class CernerDocumentReferenceService(
+    cernerClient: CernerClient,
+    @Value("\${cerner.docref.category:LP29684-5,LP29708-2,LP75011-4,LP7819-8,LP7839-6,clinical-note}")
+    categoryString: String
+) : DocumentReferenceService, CernerFHIRService<DocumentReference>(cernerClient) {
     override val fhirURLSearchPart = "/DocumentReference"
     override val fhirResourceType = DocumentReference::class.java
+
+    private val categories: List<String> = if (categoryString.isBlank()) emptyList() else categoryString.split(",")
 
     @Trace
     override fun findPatientDocuments(
@@ -24,12 +31,11 @@ class CernerDocumentReferenceService(cernerClient: CernerClient) : DocumentRefer
         endDate: LocalDate
     ):
         List<DocumentReference> {
+        val categoryParameters = if (categories.isNotEmpty()) mapOf("category" to categories) else emptyMap()
         val parameters = mapOf(
             "patient" to patientFhirId,
-            // Categories are hardcoded per INFX: INT-2014
-            "category" to listOf("LP29684-5", "LP29708-2", "LP75011-4", "LP7819-8", "LP7839-6", "clinical-note"),
             "date" to getDateParam(startDate, endDate, tenant)
-        )
+        ) + categoryParameters
         return getResourceListFromSearch(tenant, parameters)
     }
 }
--- interop-ehr-cerner/src/test/kotlin/com/projectronin/interop/ehr/cerner/CernerDocumentReferenceTest.kt
+++ interop-ehr-cerner/src/test/kotlin/com/projectronin/interop/ehr/cerner/CernerDocumentReferenceTest.kt
@@ -1,6 +1,5 @@
 package com.projectronin.interop.ehr.cerner
 
-import com.projectronin.interop.ehr.DocumentReferenceService
 import com.projectronin.interop.ehr.cerner.client.CernerClient
 import com.projectronin.interop.ehr.client.RepeatingParameter
 import com.projectronin.interop.ehr.outputs.EHRResponse
@@ -16,8 +15,7 @@ import org.junit.jupiter.api.Test
 import java.time.LocalDate
 
 class CernerDocumentReferenceTest {
-    private var cernerClient: CernerClient = mockk()
-    private var documentReferenceService: DocumentReferenceService = CernerDocumentReferenceService(cernerClient)
+    private val cernerClient: CernerClient = mockk()
 
     @Test
     fun getDocumentReferencesByPatient() {
@@ -49,13 +47,72 @@ class CernerDocumentReferenceTest {
                 "/DocumentReference",
                 mapOf(
                     "patient" to "12345",
-                    "category" to listOf("LP29684-5", "LP29708-2", "LP75011-4", "LP7819-8", "LP7839-6", "clinical-note"),
+                    "category" to listOf(
+                        "LP29684-5",
+                        "LP29708-2",
+                        "LP75011-4",
+                        "LP7819-8",
+                        "LP7839-6",
+                        "clinical-note"
+                    ),
                     "date" to RepeatingParameter(listOf("ge2015-01-01T00:00:00-06:00", "lt2015-11-02T00:00:00-06:00")),
                     "_count" to 20
                 )
             )
         } returns EHRResponse(mockk { coEvery { body<Bundle>() } returns bundle }, "12345")
 
+        val documentReferenceService = CernerDocumentReferenceService(
+            cernerClient,
+            "LP29684-5,LP29708-2,LP75011-4,LP7819-8,LP7839-6,clinical-note"
+        )
+        val response =
+            documentReferenceService.findPatientDocuments(
+                tenant,
+                "12345",
+                LocalDate.of(2015, 1, 1),
+                LocalDate.of(2015, 11, 1)
+            )
+
+        assertEquals(listOf(documentReference1.resource, documentReference2.resource), response)
+    }
+
+    @Test
+    fun `getDocumentReferencesByPatient when empty categories sent`() {
+        val tenant = createTestTenant(
+            clientId = "XhwIjoxNjU0Nzk1NTQ4LCJhenAiOiJEaWNtODQ",
+            authEndpoint = "https://example.org",
+            secret = "GYtOGM3YS1hNmRmYjc5OWUzYjAiLCJ0Z",
+            timezone = "UTC-06:00"
+        )
+
+        val documentReference1 = mockk<BundleEntry> {
+            every { resource } returns mockk<DocumentReference>(relaxed = true) {
+                every { id!!.value } returns "12345"
+            }
+        }
+        val documentReference2 = mockk<BundleEntry> {
+            every { resource } returns mockk<DocumentReference>(relaxed = true) {
+                every { id!!.value } returns "67890"
+            }
+        }
+        val bundle = mockk<Bundle>(relaxed = true) {
+            every { entry } returns listOf(documentReference1, documentReference2)
+            every { link } returns emptyList()
+        }
+
+        coEvery {
+            cernerClient.get(
+                tenant,
+                "/DocumentReference",
+                mapOf(
+                    "patient" to "12345",
+                    "date" to RepeatingParameter(listOf("ge2015-01-01T00:00:00-06:00", "lt2015-11-02T00:00:00-06:00")),
+                    "_count" to 20
+                )
+            )
+        } returns EHRResponse(mockk { coEvery { body<Bundle>() } returns bundle }, "12345")
+
+        val documentReferenceService = CernerDocumentReferenceService(cernerClient, "")
         val response =
             documentReferenceService.findPatientDocuments(
                 tenant,
